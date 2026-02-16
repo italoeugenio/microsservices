e3ec0f810cd05a99a4790b1317d62715e04beb6c
Ítalo Santana <italoeugenio539@gmail.com>
2026-02-16 18:25:23 -0300

    fix: application propeties and Emailconsumer

--- email-api/src/main/java/com/ms/email_api/consumer/EmailConsumer.java
+++ email-api/src/main/java/com/ms/email_api/consumer/EmailConsumer.java
@@ -3,16 +3,18 @@ package com.ms.email_api.consumer;
 import com.ms.email_api.application.EmailSenderService;
 import com.ms.email_api.dtos.EmailDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    @Autowired
     private EmailSenderService emailSenderService;
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailDTO emailDTO) {
-        emailSenderService.sendEmail(emailDTO.to(), emailDTO.subject(), emailDTO.body());
+        System.out.println("Mensagem recebida: " + emailDTO);
     }
 }
